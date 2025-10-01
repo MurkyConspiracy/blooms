149fa2bee8913e4c7d749ad222a1f5acbc94ef4a
MurkyConspiracy <nickreeder43@gmail.com>
2025-09-30 21:36:46 -0500

    Sounds Effects!

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -4,6 +4,13 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const DECEL = 15
 
+@onready var move_effet: AudioStreamPlayer = %MoveEffet
+@onready var thump_effect: AudioStreamPlayer = %ThumpEffect
+@onready var spread_effect: AudioStreamPlayer = %SpreadEffect
+@onready var contract_effect: AudioStreamPlayer = %ContractEffect
+@onready var boing_effect: AudioStreamPlayer = %BoingEffect
+
+
 @export var death_velocity : float = 1500
 @onready var velocity_dev: Label = $CanvasLayer/velocity_dev
 
@@ -43,19 +50,24 @@ func _physics_process(delta: float) -> void:
 	fall_dev.text = ("Fall Time: %f      Lock: %s" % [fall_timer.time_left,fall_lock])
 	velocity_dev.text = ("XVel: %f      YVel: %f" % [velocity.x, velocity.y])
 	floor_dev.text = "Is On Floor: %s" % str(is_on_floor())
-	
+	if velocity.y > 1000:
+		velocity.y = move_toward(velocity.y, 1000, 1 * (velocity.y - 1000))
+		
 	
 	var collision = move_and_collide(velocity * delta, true)
-
 	if collision:
-		if collision.get_angle() < deg_to_rad(15):
+		if collision.get_angle() < deg_to_rad(5):
 			if (collision.get_collider() is AnimatableBody2D && 
 				(collision.get_collider() as AnimatableBody2D).physics_material_override.resource_path == "res://scenes/level_components/bounce_platform.tres"):
+					boing_effect.pitch_scale = lerp(0.8, 1.5, 1- clamp(abs(velocity.y) / 1000, 0, 1))
+					boing_effect.play()
 					velocity = velocity.bounce(collision.get_normal().normalized()) * Vector2(1, (collision.get_collider() as AnimatableBody2D).physics_material_override.bounce)
 					apply_floor_snap()
 					fall_timer.stop()
 					left_dash_lock = false
 					right_dash_lock = false
+			else:
+				thump_effect.play()
 
 	# Add the gravity and handle float.
 	if not is_on_floor() && !fall_start:
@@ -100,6 +112,7 @@ func _physics_process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("float") && !float_lock && not is_on_floor():
+		spread_effect.play()
 		if float_timer.is_stopped():
 			float_timer.start(float_duration)
 			float_timer.set_paused(false)
@@ -109,6 +122,7 @@ func _input(event: InputEvent) -> void:
 			fall_timer.set_paused(true)
 	elif event.is_action_released("float") && not fall_timer.is_stopped():
 		fall_timer.set_paused(false)
+		contract_effect.play()
 		if !float_timer.is_stopped():
 			float_timer.set_paused(true)
 		if fall_timer.is_stopped() && !fall_lock:
@@ -121,18 +135,24 @@ func _input(event: InputEvent) -> void:
 		velocity.y += -120
 		velocity.x = -left_dash_velocity
 		left_dash_lock = true
+	if (event.is_action_pressed("move_left") || event.is_action_pressed("move_right")):
+		move_effet.play()
+	elif event.is_action_released("move_left") && not Input.is_action_pressed("move_right") && move_effet.playing:
+		move_effet.stop()
+	elif event.is_action_released("move_right") && not Input.is_action_pressed("move_left") && move_effet.playing:
+		move_effet.stop()
+	elif Input.is_action_pressed("move_left") && Input.is_action_pressed("move_right") && velocity == Vector2.ZERO:
+		move_effet.stop()
+	if not is_on_floor() && move_effet.playing:
+		move_effet.stop()
+		
 
 func _on_float_timer_timeout() -> void:
 	float_lock = true
 
 func _on_fall_timer_timeout() -> void:
 	fall_lock = true
-	
-func reset_player():
-	velocity = Vector2.ZERO
-	global_position = DataHandler.spawn_position
-	reset_conditionals()
-	
+		
 func reset_conditionals():
 	float_timer.set_paused(false)
 	float_timer.stop()
--- scenes/utils/data_handler.gd
+++ scenes/utils/data_handler.gd
@@ -14,16 +14,21 @@ func damage_player(damage_amount : int = 1, force_death : bool = false) -> bool:
 	if player_health >= 0:
 		player_reference.global_position = spawn_position
 		player_camera.call("reset_camera_pos")
+		reset_input_mapping()
 		return false
 	elif force_death || player_health < 0:
 		player_health = 4
+		reset_input_mapping()
 		get_tree().reload_current_scene()
 		return true
-	else:
-		assert("How did we get here!")
 	player_camera.call("reset_camera_pos")
 	return false
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit_game"):
 		get_tree().quit(0)
+		
+func reset_input_mapping() -> void:
+	for action in InputMap.get_actions():
+		if Input.is_action_pressed(action):
+			Input.action_release(action)
